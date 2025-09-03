6a0e5b9d483c0d1229099ae08a4991a9b9ba578d
Luis Lemos Jr <luisc.jr@gmail.com>
2025-09-02 23:49:45 -0300

    refinando código

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //obter o o carrinho decompras
 let carrinho = document.getElementById("lista-produtos");
+limpar();//inicializando com o carrinho limpo
 
 function adicionar() {
 
@@ -8,25 +9,37 @@ function adicionar() {
 
     let item = valor.split(' - ');//separando nome do valor
     let nome = item[0];
-    let preco = item[1].replace('R$','');    
+    let preco = item[1].replace('R$','');
+    let qtd = document.getElementById('quantidade').value;
 
-    let sessao = document.createElement("section");//cria um elemento do tipo sessão
-    let qtd = document.getElementById('quantidade')?.value||'1';//se o campo quantidade não estiver preenchido,será 1 por padrão
+    //validando o campo de quantidade
+    //verifica se numero é maior que zero ou se não é numerico
+    if(qtd < 1 || isNaN(parseInt(qtd)) || isNaN(parseFloat(qtd))){
+        alert('Quantidade deve ser um númeroválido maior que zero!');
+        return;
+    }
+    //let qtd = document.getElementById('quantidade')?.value||'1';//se o campo quantidade não estiver preenchido, será 1 por padrão
+   
+    //não precisa, neste caso, declarar a variável total fora da função, pois o código aqui, pega o valor total atual já escrito na tela
     let total = document.getElementById('valor-total').innerHTML.replace('R$','');//obtem o valor total
-    sessao.classList.add("carrinho__produtos__produto");//adiciona estilo
+    let valorTotal = parseFloat(total)+(parseFloat(preco.toFixed(2))*parseFloat(qtd));//montando valor total
 
-    //montando tag
-    sessao.innerHTML = `<span class="texto-azul">${qtd}x</span> ${nome} <span class="texto-azul">R$${preco}</span>`;
-
-    carrinho.appendChild(sessao);//adicionando tag
-    let valorTotal = parseInt(total)+(parseInt(preco)*parseInt(qtd));//montando valor total
+    //inserindo novos itens no carrinho
+    //observe a necessidade de concatenação de Sections para que o novo item não sobrescreva o antigo
+    carrinho.innerHTML += `<section class="carrinho__produtos__produto"> <span class="texto-azul">${qtd}x`+
+    `</span> ${nome} <span class="texto-azul">R$ ${preco}</span> </section>`;
 
     document.getElementById('valor-total').innerHTML = `R$ ${valorTotal}`;//inserindo valor total
+    document.getElementById('quantidade').value = '';
 }
 
 function limpar() {
     // Seleciona todos os produtos dentro do carrinho
-    let produtos = carrinho.querySelectorAll('.carrinho__produtos__produto');
-    produtos.forEach(produto => produto.remove());//para cada elemento do carrinho, remova esse elemento
+    let carrinho = document.getElementById("lista-produtos").innerHTML = '';
+
+    //outra abordagem valida para fins didaticos
+    // let produtos = carrinho.querySelectorAll('.carrinho__produtos__produto');
+    // produtos.forEach(produto => produto.remove());//para cada elemento do carrinho, remova esse elemento
+    
     document.getElementById('valor-total').innerHTML = 'R$ 0';//limpando valor total
 }
